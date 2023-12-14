9a286759c3880177c76cf239245bdb1cad0c7224
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-12-14 12:12:32 -0300

    Update BtnAcept.java
    s

--- src/botones/BtnAcept.java
+++ src/botones/BtnAcept.java
@@ -3,18 +3,26 @@
  */
 package botones;
 
+// Importa la clase iconos del paquete clinica.FrmSistema
 import static clinica.FrmSistema.iconos;
 
-
 /**
- *
- * @author pablo
+ * Clase que representa un botón de tipo "Aceptar".
+ * Extiende la clase Btn para heredar características de botón base.
  */
-public class BtnAcept extends Btn{
+public class BtnAcept extends Btn {
     
-    public BtnAcept(){
+    /**
+     * Constructor de la clase BtnAcept.
+     * Configura el texto del botón como "Aceptar" y establece un icono.
+     */
+    public BtnAcept() {
+        // Configura el texto del botón
         setText("Aceptar");
+        
+        // Configura el icono del botón utilizando el objeto iconos y su método getAcept(16)
         setIcon(iconos.getAcept(16));
     }
     
+}
 }
