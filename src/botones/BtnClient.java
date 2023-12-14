d4dd02a236e13948408185806e2a95a737e2c4f7
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-12-14 12:13:21 -0300

    Update BtnClient.java

--- src/botones/BtnClient.java
+++ src/botones/BtnClient.java
@@ -3,17 +3,26 @@
  */
 package botones;
 
+// Importa la clase iconos del paquete clinica.FrmSistema
 import static clinica.FrmSistema.iconos;
 
 /**
- *
- * @author pablo
+ * Clase que representa un botón de tipo "Cliente".
+ * Extiende la clase Btn para heredar características de botón base.
  */
-public class BtnClient extends Btn{
+public class BtnClient extends Btn {
     
-    public BtnClient(){
+    /**
+     * Constructor de la clase BtnClient.
+     * Configura el texto del botón como "Cliente" y establece un icono.
+     */
+    public BtnClient() {
+        // Configura el texto del botón
         setText("Cliente");
+        
+        // Configura el icono del botón utilizando el objeto iconos y su método getClient(16)
         setIcon(iconos.getClient(16));
     }
     
 }
+
