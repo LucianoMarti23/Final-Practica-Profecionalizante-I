b595f8fbd4c78d314acb3981eb3a5351ccb2cd9a
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-12-14 12:16:11 -0300

    Update BtnFilter.java

--- src/botones/BtnFilter.java
+++ src/botones/BtnFilter.java
@@ -3,18 +3,26 @@
  */
 package botones;
 
+// Importa la clase iconos del paquete clinica.FrmSistema
 import static clinica.FrmSistema.iconos;
 
-
 /**
- *
- * @author pablo
+ * Clase que representa un botón de tipo "Filtro".
+ * Extiende la clase Btn para heredar características de botón base.
  */
-public class BtnFilter extends Btn{
+public class BtnFilter extends Btn {
     
-    public BtnFilter(){
+    /**
+     * Constructor de la clase BtnFilter.
+     * Configura el texto del botón como "Filtro" y establece un icono.
+     */
+    public BtnFilter() {
+        // Configura el texto del botón
         setText("Filtro");
+        
+        // Configura el icono del botón utilizando el objeto iconos y su método getFilter(16)
         setIcon(iconos.getFilter(16));
     }
     
 }
+
