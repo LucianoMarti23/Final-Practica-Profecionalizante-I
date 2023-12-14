8ed80592acf930b1d99b366cbc32cb9cb010dae3
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-12-14 12:13:59 -0300

    Update BtnDelete.java

--- src/botones/BtnDelete.java
+++ src/botones/BtnDelete.java
@@ -3,13 +3,26 @@
  */
 package botones;
 
+// Importa la clase iconos del paquete clinica.FrmSistema
 import static clinica.FrmSistema.iconos;
 
-
+/**
+ * Clase que representa un botón de tipo "Borrar".
+ * Extiende la clase Btn para heredar características de botón base.
+ */
 public class BtnDelete extends Btn {
     
-    public BtnDelete(){
+    /**
+     * Constructor de la clase BtnDelete.
+     * Configura el texto del botón como "Borrar" y establece un icono.
+     */
+    public BtnDelete() {
+        // Configura el texto del botón
         setText("Borrar");
+        
+        // Configura el icono del botón utilizando el objeto iconos y su método getDelete(16)
         setIcon(iconos.getDelete(16));
     }
+    
 }
+
