5b847eae1db58adc9a61353bc6e932c8886aa4bc
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-12-14 12:14:33 -0300

    Update BtnEdit.java

--- src/botones/BtnEdit.java
+++ src/botones/BtnEdit.java
@@ -3,13 +3,25 @@
  */
 package botones;
 
+// Importa la clase iconos del paquete clinica.FrmSistema
 import static clinica.FrmSistema.iconos;
 
-
+/**
+ * Clase que representa un botón de tipo "Editar".
+ * Extiende la clase Btn para heredar características de botón base.
+ */
 public class BtnEdit extends Btn {
     
-    public BtnEdit(){
+    /**
+     * Constructor de la clase BtnEdit.
+     * Configura el texto del botón como "Actualizar" y establece un icono.
+     */
+    public BtnEdit() {
+        // Configura el texto del botón
         setText("Actualizar");
+        
+        // Configura el icono del botón utilizando el objeto iconos y su método getEdit(16)
         setIcon(iconos.getEdit(16));
     }
+    
 }
