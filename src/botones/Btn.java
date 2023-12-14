8f08412d979b3759476144f291daa86182470436
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-12-14 12:10:40 -0300

    Update Btn.java

--- src/botones/Btn.java
+++ src/botones/Btn.java
@@ -1,14 +1,17 @@
 /*
- * Clase base o super clase para la creacion de los botones
+ * Clase base o super clase para la creación de los botones
  */
 package botones;
+
+// Importa la clase JButton del paquete javax.swing
 import javax.swing.JButton;
 
-
-
-public class Btn extends JButton{
+// Definición de la clase Btn que extiende JButton
+public class Btn extends JButton {
     
-    public Btn (){
-        setFont(new java.awt.Font("Thaoma",1 ,10));
+    // Constructor de la clase Btn
+    public Btn() {
+        // Establece la fuente del botón con un nuevo objeto de la clase Font
+        setFont(new java.awt.Font("Thaoma", 1, 10));
     }
 }
