21bc9a29d9aeca71349ba24d6b2a23a2fc5d2c03
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-12-14 12:15:09 -0300

    Update BtnExit.java

--- src/botones/BtnExit.java
+++ src/botones/BtnExit.java
@@ -3,14 +3,26 @@
  */
 package botones;
 
+// Importa la clase iconos del paquete clinica.FrmSistema
 import static clinica.FrmSistema.iconos;
 
-
-public class BtnExit extends Btn{
+/**
+ * Clase que representa un botón de tipo "Salir".
+ * Extiende la clase Btn para heredar características de botón base.
+ */
+public class BtnExit extends Btn {
     
-    public BtnExit(){
+    /**
+     * Constructor de la clase BtnExit.
+     * Configura el texto del botón como "Salir" y establece un icono.
+     */
+    public BtnExit() {
+        // Configura el texto del botón
         setText("Salir");
+        
+        // Configura el icono del botón utilizando el objeto iconos y su método getExit(16)
         setIcon(iconos.getExit(16));
     }
     
 }
+
